c3cf1c9d36e3d8f59698f8a3a49244d62a537109
Mario1998i <mario.parisella@outlook.it>
2026-09-17 11:52:13 +0200

    feat: add Node Express backend and Docker

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.API_URL || "http://localhost:3000";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
 
 export async function fetchBooksByCategory(category) {
     const url = `${API_URL}/api/books?category=${category}`;
